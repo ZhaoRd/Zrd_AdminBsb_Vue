d796eb37098b8f279f28a29280fafd5e282dfc78
Rende Zhao <351157970@qq.com>
2017-06-25 00:33:55 +0800

    完成adminlte 的搭建

--- src/main.js
+++ src/main.js
@@ -4,6 +4,18 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap/dist/js/bootstrap.min.js'
+
+// AdminLTE
+import './assets/adminlte/css/skins/_all-skins.min.css'
+import './assets/adminlte/css/AdminLTE.min.css'
+import './assets/adminlte/js/app.min.js'
+
+// css
+import 'font-awesome/css/font-awesome.min.css'
+import 'ionicons/css/ionicons.css'
+
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
